2c9ec932714a82ed890c71afc42e7ff6b2aa2ec5
Kenny Saenz <ksaenz@bi.com.gt>
2024-10-26 00:19:32 -0600

    MOD: Componentes del Home

---
+++ src/Utils/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+// Utils/PrivateRoute.js
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
+
+const PrivateRoute = () => {
+  
+  const token = localStorage.getItem("token");
+
+  
+  return token ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default PrivateRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {BrowserRouter as Router,Routes,Route,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./routes/home";
 import Login from "./routes/login";
 import Register from "./routes/Register";
-//import PrivateRoute from "./components/PrivateRoute"; 
-//<Route element={<PrivateRoute />}>
-//</Route>
-
+import PrivateRoute from "./Utils/PrivateRoute";
 
 const AppRouter = () => {
   return (
@@ -16,9 +13,13 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/register" element={<Register />}/>
         
+        {/* Rutas protegidas usando PrivateRoute */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+        
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
